8e2c236630f5de21ee02e5e29430e3492934de46
AshPark <anpp77@mst.edu>
2015-04-30 15:40:54 -0500

    tester.php
    This is a cleaner test file. It does 3 simple queries. The error handling is bad and you can only do one function at a time but it is more that the first file I submitted. The tester1.php file is also needed to run this!

---
+++ tester.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+<form action="tester1.php" method="post">
+What type of pet are you looking for: <select name="type">
+  <option value="">Select...</option>
+  <option value="Mammal">Mammal</option>
+  <option value="Fish">Fish</option>
+  <option value="Bird">Bird</option>
+  <option value="Reptile">Reptile</option>
+</select>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
+Find a caretaker
+<form name="FindCare" action="tester1.php" method="post">
+Enter the ID of the pet:  <input type="text" name="pid" id="pid" value="">
+</form>
+
+Find the food a pet likes
+<form name="FindFood" action="tester1.php" method="post">
+Enter the ID of the pet:  <input type="text" name="pid1" id="pid1" value="">
+</form>
