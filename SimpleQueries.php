b097eb46da2d9563f5e3e6ebd524247d6e9d2c6c
Aaron Joyce <aronwanderer@gmail.com>
2015-04-30 16:37:17 -0500

    Messed with styling to  make it more readable

--- SimpleQueries.php
+++ SimpleQueries.php
@@ -4,39 +4,13 @@
 	<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	</head>
 
+<h1>
+	Welcome to Goats and Giggles
+</h1>
+
 <body>
-	<?php
-    $username="root";$password="1234";$database="Goats";
-	#mysql_connect('localhost');
-	mysql_connect('localhost',$username,$password);
-	@mysql_select_db($database) or die( "Unable to select database");
-	
-  	# operator
-	print "<h1>Welcome to Goats and Giggles</h1>";
 
-	$type = $_GET['subject'];
-	
-	$result = mysql_query("SELECT * from $type");
-	$num = mysql_numrows($result);
-	
-	echo $num;
-	
-	$i=0;
-	while($i<$num)
-	{
-	   $answer = mysql_result($result,$i);
-	   echo $answer;
-	   echo "\n";
-	   $i =$i+1;
-	}
-	#echo mysql_query("SELECT PID from pet");
-	
-	mysql_close();
-?>
-
-
-
-
+<div class="text">
 <form action="tester1.php" method="post">
 What type of pet are you looking for: <select name="type">
   <option value="">Select...</option>
@@ -48,8 +22,6 @@ What type of pet are you looking for: <select name="type">
 <input type="submit">
 </form>
 
-</body>
-</html>
 
 Find a caretaker
 <form name="FindCare" action="tester1.php" method="post">
@@ -60,3 +32,6 @@ Find the food a pet likes
 <form name="FindFood" action="tester1.php" method="post">
 Enter the ID of the pet:  <input type="text" name="pid1" id="pid1" value="">
 </form>
+</div>
+</body>
+</html>
