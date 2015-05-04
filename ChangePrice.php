08a223597f91283b11a7f5713518ecdc8a41ae3a
Deborah Yu <dyrg4@mst.edu>
2015-05-04 02:22:29 -0500

    a disgusting piece of code that works, but is horrifyingly ugly

---
+++ ChangePrice.php
@@ -0,0 +1,60 @@
+<html>
+<body>
+Please fill out the following information to change the price of an item or pet in the database: <br><br>
+
+<form name = "changePrice" action="" method="post">
+ID: <input type="text" name="ID" id="ID"><br>
+New Price: $<input type="text" name="Price" id="Price"><br><br>
+<input type="submit" name = "changePrice" id = "changePrice" value = "Submit">
+</form>
+
+<?php
+	//Connect to Database
+	error_reporting( error_reporting() & ~E_NOTICE );
+	$username="root";$password="1234";$database="hw3";
+	mysql_connect('localhost',$username,$password);
+	@mysql_select_db($database) or die( "Unable to select database");
+
+	$ID = $_POST['ID'];
+	$Price = $_POST['Price'];
+	$IDType = 'FID';
+	$searchType = 'Food';
+
+	$sqlChecker = "SELECT * from `$searchType`
+					WHERE `$IDType` = '$ID';"; 
+
+		$Updater = "UPDATE `$searchType`
+       		SET `Price` = $Price 
+       		WHERE `$IDType` = '$ID';";
+       			mysql_query($Updater);
+
+	//Checks if valid ID
+	if(mysql_num_rows(mysql_query($sqlChecker)) == 0)
+	{
+		$searchType = 'Toy';
+		$IDType = 'TID';
+			$Updater = "UPDATE `$searchType`
+       		SET `Price` = $Price 
+       		WHERE `$IDType` = '$ID';";
+       			mysql_query($Updater);
+	}
+	if(mysql_num_rows(mysql_query($sqlChecker)) == 0)
+	{
+		$searchType = 'Pet';
+			$IDType = 'PID';
+				$Updater = "UPDATE `$searchType`
+       		SET `Price` = $Price 
+       		WHERE `$IDType` = '$ID';";
+       			mysql_query($Updater);
+	}
+
+	//Updates Price of item, once found
+	$Updater = "UPDATE `$searchType`
+       		SET `Price` = $Price 
+       		WHERE `$IDType` = '$ID';";
+
+	mysql_query($Updater);
+	mysql_close();
+?>
+</body>
+</html>
